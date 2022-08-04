4846d36bec72bf6d180015aa83514eca2ccc32bb
julidiv <julia.dzivina@gmail.com>
2022-08-04 18:04:14 +0200

    Initial Commit

---
+++ SpringAnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAnimationApp
+//
+//  Created by Alexander Golub on 4.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
